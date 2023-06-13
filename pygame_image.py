018f5337689179d5b0708da78713d0dcf001a971
YuuIde <c0a2201673@edu.teu.ac.jp>
2023-06-13 16:44:14 +0900

    演習問題完了

--- pygame_image.py
+++ pygame_image.py
@@ -6,25 +6,38 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
+    
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
+    
     kt_img = pg.image.load("ex01/fig/3.png")
     kt_img = pg.transform.flip(kt_img, True, False)
-    kt_imgs = [kt_img, pg.transform.rotozoom(kt_img, 10, 1.0)]
+    kt_imgs = [pg.transform.rotozoom(kt_img, i, 1.0) for i in range(11)]
+    
     tmr = 0
-
+    kt_mv = 0
+    mv_num = 1
+    flipFlag = True
     
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [-tmr, 0])
-        screen.blit(bg_img,[-tmr+1600,0])
-        screen.blit(kt_imgs[tmr%2], [300, 200])
+        screen.blit(bg_img2,[-tmr+1600, 0])
+        screen.blit(pg.transform.rotozoom(kt_img, kt_mv, 1.0), [300, 200])
         pg.display.update()
         tmr += 1
+        kt_mv += mv_num
+        
+        if(kt_mv > 10 or kt_mv < 0):
+            mv_num *= -1
         
         if(tmr > 1600):
-            tmr = 0  
+            tmr = 0
+            bg_img = pg.transform.flip(bg_img, flipFlag, False)
+            bg_img2 = pg.transform.flip(bg_img, -flipFlag, False)
+            flipFlag = -flipFlag 
                    
         clock.tick(100)
 
